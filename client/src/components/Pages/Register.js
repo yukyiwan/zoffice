7876bd5ee8901fb664e394bce8413a112185cf45
yukyiwan <71396331+yukyiwan@users.noreply.github.com>
2021-04-06 20:44:57 -0400

    Initial setup
    Running reg, login, and landing-post-login

---
+++ client/src/components/Pages/Register.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+import './Register.scss';
+import FormReg from '../Form/FormReg';
+import ListPersons from '../List/Persons';
+import { getPersons } from '../../actions/persons'
+
+const Register = () => {
+    // react-redux hook for actions // useEffect is use to mount state, later on will become the component update
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPersons());
+    }, [dispatch]);
+
+    return ( 
+        <Container fluid align="center">
+            <h1>ZOffice</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <FormReg />
+                    </Col>
+                    <Col>
+                        <ListPersons />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+     );
+}
+ 
+export default Register;
